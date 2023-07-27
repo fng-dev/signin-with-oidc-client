ccfd0a0bab65fafaf9e1aac857fdad35b6ff41da
Franco Nascimento <fng.desenvolvedor@gmail.com>
2023-07-27 16:18:27 -0300

    chore: move config to enviroment file

--- src/configs/authConfig.ts
+++ src/configs/authConfig.ts
@@ -1,9 +1,9 @@
 const authConfig: any = {
   client_id: process.env.REACT_APP_CLIENT_ID,
-  authority: "https://copec-sa.fusionauth.io/",
-  token_endpoint: "https://copec-sa.fusionauth.io/oauth2/token",
-  redirect_uri: "http://localhost:7001/auth/callback",
-  logout_uri: "http://localhost:7001/logout",
+  authority: process.env.REACT_APP_AUTHORITY,
+  token_endpoint: process.env.REACT_APP_TOKEN_ENDPOINT,
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  logout_uri: process.env.REACT_APP_LOGOUT_URI,
   response_type: "code",
   scope: "openid profile email userId refresh_token offline_access",
   userinfo_endpoint: "oauth2/userinfo",
